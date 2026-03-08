1c914e0b8a71ddf6023fade697d113f152c94761
Faried Idris <107062781+psyofrelief@users.noreply.github.com>
2026-03-08 20:09:22 +0800

    fix: modify auth declaration

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,16 +1,14 @@
-import { DefaultSession, DefaultUser } from "next-auth";
+import { DefaultSession } from "next-auth";
 import { JWT as DefaultJWT } from "next-auth/jwt";
 
 declare module "next-auth" {
-  interface User extends DefaultUser {
-    emailVerified: Date | null;
-    hasPassword: boolean;
+  interface User {
+    hasPassword?: boolean;
   }
 
   interface Session {
     user: {
       id: string;
-      emailVerified: Date | null;
       hasPassword: boolean;
     } & DefaultSession["user"];
   }
@@ -18,8 +16,6 @@ declare module "next-auth" {
 
 declare module "next-auth/jwt" {
   interface JWT extends DefaultJWT {
-    id: string;
-    emailVerified: Date | null;
-    hasPassword: boolean;
+    hasPassword?: boolean;
   }
 }
